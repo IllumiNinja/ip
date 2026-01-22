827a9d947624c47a1b70e814e2f79fb5fc1f5b87
IllumiNinja <42295723+IllumiNinja@users.noreply.github.com>
2026-01-22 23:13:24 +0800

    Level 2. Add, List

--- src/main/java/Talkative.java
+++ src/main/java/Talkative.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 
 public class Talkative {
+    private static final int MAX_TASKS = 100;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] UItasks = new String[MAX_TASKS];
+        int taskCount = 0;
 
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Talkative");
@@ -18,12 +21,23 @@ public class Talkative {
                 break;
             }
 
+            if (userInput.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + UItasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+                continue;
+            }
+
+            UItasks[taskCount] = userInput;
+            taskCount++;
+
             System.out.println("____________________________________________________________");
-            System.out.println(" " + userInput);
+            System.out.println(" added: " + userInput);
             System.out.println("____________________________________________________________");
         }
 
         scanner.close();
-
     }
 }
