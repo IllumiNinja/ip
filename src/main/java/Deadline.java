cc8cf5646d4c8cb0bfb5cf2ac75f8bade50455d5
IllumiNinja <42295723+IllumiNinja@users.noreply.github.com>
2026-01-30 13:22:40 +0800

    Change string dates to use LocalDate

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,9 +1,17 @@
-public class Deadline extends Task {
-    protected String deadlineBy;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String description, String deadlineBy) {
+public class Deadline extends Task {
+    protected LocalDate deadlineBy;
+
+    public Deadline(String description, String deadlineBy) throws TalkativeException {
         super(description);
-        this.deadlineBy = deadlineBy;
+
+        try {
+            this.deadlineBy = LocalDate.parse(deadlineBy);
+        } catch (Exception e) {
+            throw new TalkativeException("Use date format yyyy-mm-dd (e.g., 2019-10-15)");
+        }
     }
 
     /**
@@ -13,12 +21,20 @@ public class Deadline extends Task {
      */
     @Override
     public String toFileFormat() {
-        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + deadlineBy;
+        return "D | "
+                + (isDone ? "1" : "0")
+                + " | "
+                + description
+                + " | " + deadlineBy.toString();
     }
 
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + deadlineBy + ")";
+        return "[D]"
+                + super.toString()
+                + " (by: "
+                + deadlineBy.format(DateTimeFormatter.ofPattern("MMM d yyyy"))
+                + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,11 +1,19 @@
-public class Event extends Task {
-    protected String eventFrom;
-    protected String eventTo;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String description, String eventFrom, String eventTo) {
+public class Event extends Task {
+    protected LocalDate  eventFrom;
+    protected LocalDate eventTo;
+
+    public Event(String description, String eventFrom, String eventTo) throws TalkativeException {
         super(description);
-        this.eventFrom = eventFrom;
-        this.eventTo = eventTo;
+
+        try {
+            this.eventFrom = LocalDate.parse(eventFrom);
+            this.eventTo = LocalDate.parse(eventTo);
+        } catch (Exception e) {
+            throw new TalkativeException("Use date format yyyy-mm-dd (e.g., 2019-10-15)");
+        }
     }
 
     /**
@@ -15,13 +23,24 @@ public class Event extends Task {
      */
     @Override
     public String toFileFormat() {
-        return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + eventFrom + " | " + eventTo;
+        return "E | "
+                + (isDone ? "1" : "0")
+                + " | "
+                + description
+                + " | "
+                + eventFrom.toString()
+                + " | "
+                + eventTo.toString();
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString()
-                + " (from: " + eventFrom + " to: " + eventTo + ")";
+        return "[E]"
+                + super.toString()
+                + " (from: "
+                + eventFrom.format(DateTimeFormatter.ofPattern("MMM d yyyy"))
+                + " to: "
+                + eventTo.format(DateTimeFormatter.ofPattern("MMM d yyyy"))
+                + ")";
     }
-
 }
